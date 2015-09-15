fa67a22f9639502b8965cf3ee4ba14ed6f13be9f
Pablo Alcain <pabloalcain@gmail.com>
2015-09-14 22:19:56 -0300

    max en r, g, b. falta el valor de alpha

--- tp2/filtros/diff_asm.asm
+++ tp2/filtros/diff_asm.asm
@@ -4,19 +4,42 @@ global diff_asm
 
 
 section .data
-
-
+mask_rot: DB 1, 2, 0, 0xFF, 5, 6, 4, 0xFF
 
 section .text
-;void diff_asm    (
-  ;unsigned char *src,
-  ;unsigned char *src2,
-  ;unsigned char *dst,
-  ;int filas,
-  ;int cols)
-
-_diff_asm:
+;void diff_asm (unsigned char *src, unsigned char *src2,
+;              (       RDI        ,        RSI         ,        
+;               unsigned char *dst, int filas, int cols)
+;                      RDX        ,    RCX   ,    R8   )
 diff_asm:
+  push rbp
+  mov rbp, rsp
+  xor r10, r10
+  movq xmm2, [mask_rot]
+  .filas:
+    xor r9, r9
+    .columnas:
+      movq xmm0, [rdi]
+      movq xmm1, [rsi]
+      psubb xmm0, xmm1
+      pabsb xmm0, xmm0
+      movq xmm1, xmm0
+      pshufb xmm1, xmm2
+      pmaxub xmm0, xmm1 
+      pshufb xmm1, xmm2
+      pmaxub xmm0, xmm1 
 
+      movq [rdx], xmm0
 
-    ret
+      add rsi, 8
+      add rdi, 8
+      add rdx, 8
+      add r9, 2                 ; procesamos dos pixeles a la vez
+      cmp r9, r8
+      jnz .columnas
+    inc r10
+    cmp r10, rcx
+    jnz .filas
+
+  pop rbp
+  ret
