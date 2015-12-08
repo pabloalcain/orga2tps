46600e91973bce4eb934d24c63a153f32fea93d5
Pablo Alcain <pabloalcain@gmail.com>
2015-12-07 22:54:14 -0300

    Agregadas macros para ISR

--- tp3/src/isr.asm
+++ tp3/src/isr.asm
@@ -35,146 +35,54 @@ extern screen_guardar_estado_actual_pantalla
 ;;
 ;; Definición de MACROS
 ;; -------------------------------------------------------------------------- ;;
-MSG_ISR0  db 'Divide Error',0
-MSG_ISR1  db 'RESERVED',0
-MSG_ISR2  db 'NMI Interrupt',0
-MSG_ISR3  db 'Breakpoint',0
-MSG_ISR4  db 'Overflow',0
-MSG_ISR5  db 'BOUND Range Exceeded',0
-MSG_ISR6  db 'Invalid Opcode (Undefined Opcode)',0
-MSG_ISR7  db 'Device Not Available',0
-MSG_ISR8  db 'Double Fault',0
-MSG_ISR9  db 'Coprocessor Segment Overrun',0
-MSG_ISR10 db 'Invalid TSS',0
-MSG_ISR11 db 'Segment Not Present',0
-MSG_ISR12 db 'Stack-Segment Fault',0
-MSG_ISR13 db 'General Protection Exception',0
-MSG_ISR14 db 'Page Fault',0
-MSG_ISR15 db 'Reserved',0
-MSG_ISR16 db 'x87 FPU Error',0
-MSG_ISR17 db 'Alignment Check',0
-MSG_ISR18 db 'Machine Check',0
-MSG_ISR19 db 'SIMD FP Exception',0
+msg0: db 'Int 0: Division by zero'
+len0 equ $ - msg0
+msg1: db 'Int 1: Debugger'
+len1 equ $ - msg1
+msg2: db 'Int 2: Non-maskable'
+len2 equ $ - msg2
+msg3: db 'Int 3: Breakpoint'
+len3 equ $ - msg3
+msg4: db 'Int 4: Overflow'
+len4 equ $ - msg4
+msg5: db 'Int 5: Bounds'
+len5 equ $ - msg5
+msg6: db 'Int 6: Invalid Opcode'
+len6 equ $ - msg6
+msg7: db 'Int 7: Coprocessor not available'
+len7 equ $ - msg7
+msg8: db 'Int 8: Double fault'
+len8 equ $ - msg8
+msg9: db 'Int 9: Coprocessor segment overrun'
+len9 equ $ - msg9
+msg10: db 'Int 10: Invalid Task State Segment'
+len10 equ $ - msg10
+msg11: db 'Int 11: Segment not present'
+len11 equ $ - msg11
+msg12: db 'Int 12: Stack fault'
+len12 equ $ - msg12
+msg13: db 'Int 13: General protection fault'
+len13 equ $ - msg13
+msg14: db 'Int 14: Page fault'
+len14 equ $ - msg14
+msg15: db 'Int 15: reserved'
+len15 equ $ - msg15
+msg16: db 'Int 16: Math fault'
+len16 equ $ - msg16
+msg17: db 'Int 17: Alignment check'
+len17 equ $ - msg17
+msg18: db 'Int 18: Machine check'
+len18 equ $ - msg18
+msg19: db 'Int 19: SIMD floating point exception'
+len19 equ $ - msg19
 
-%macro ISR 3
-
+%macro ISR 1
 global _isr%1
 
 _isr%1:
-   push %2
-    pushad
-    pushfd
-
-    pop eax
-    push ebx
-    mov ebx, eax
-;    call screen_guardar_estado_actual_pantalla
-    mov eax, ebx
-    pop ebx
-    ; asumo que modo_debug recibe como parametro lo que hay en eax
-    ; MODO DEBUG
-
-        mov [guardo_eflags], eax
-        popad
-        mov dword [guardo_debug_tss + 0], 0
-        mov dword [guardo_debug_tss + 4], esp
-        mov word [guardo_debug_tss + 8], ss
-        mov word [guardo_debug_tss + 10], 0
-        mov dword [guardo_debug_tss + 12], 0
-        mov dword [guardo_debug_tss + 16], 0
-        mov dword [guardo_debug_tss + 20], 0
-        mov dword [guardo_debug_tss + 24], 0
-        mov dword [guardo_debug_tss + 40], eax
-        mov eax, cr3
-        mov dword [guardo_debug_tss + 28], eax
-        mov eax, _isr%1
-        mov dword [guardo_debug_tss + 32], eax
-        mov eax, [guardo_eflags]
-        mov dword [guardo_debug_tss + 36], eax
-        mov dword [guardo_debug_tss + 44], ecx
-        mov dword [guardo_debug_tss + 48], edx
-        mov dword [guardo_debug_tss + 52], ebx
-        mov dword [guardo_debug_tss + 56], 0
-        mov dword [guardo_debug_tss + 60], ebp
-        mov dword [guardo_debug_tss + 64], esi
-        mov dword [guardo_debug_tss + 68], edi
-        mov word [guardo_debug_tss + 72], es
-        mov word [guardo_debug_tss + 74], 0
-        mov word [guardo_debug_tss + 76], cs
-        mov word [guardo_debug_tss + 78], 0
-        mov word [guardo_debug_tss + 80], 0
-        mov word [guardo_debug_tss + 82], 0
-        mov word [guardo_debug_tss + 84], ds
-        mov word [guardo_debug_tss + 86], 0
-        mov word [guardo_debug_tss + 88], fs
-        mov word [guardo_debug_tss + 90], 0
-        mov word [guardo_debug_tss + 92], gs
-        mov word [guardo_debug_tss + 94], 0
-        mov dword [guardo_debug_tss + 96], 0
-        mov dword [guardo_debug_tss + 100], 0
-
-        ; preservo ebx, edi, esi
-        push ebx 
-        push edi
-        push esi
-
-        mov ebx, guardo_debug_tss
-
-        ;push eax
-        ; if(modo_debug == TRUE){
-        ;     screen_mostrar_cartel(debug_jugador_actual, debug_tipo_perro, debug_tss);
-        ;     pausa = TRUE;
-        ; }
-        call game_get_is_modo_debug
-        ;en eax esta el booleano modo_debug
-        cmp eax, 0 ; FALSE
-        je .no_imprimir_cartel
-
-        call sched_tarea_actual
-        ; en eax tengo el perro actual
-        mov edi, eax            ; salvo perro en edi
-
-        push eax
-        call game_get_tipo_perro
-        add esp, 4
-
-        mov esi, eax            ;salvo tipo perro
-
-        mov eax, edi            ; vuelvo a traer perro de edi
-        push eax
-        call game_get_jugador_from_perro
-
-        add esp, 4
-
-        push ebx
-        push esi
-        push eax
-        call screen_mostrar_cartel
-        add esp, 12
-
-        call set_pausa      ; pausa = TRUE
-        .no_imprimir_cartel:
-
-        ; popeo ebx, edi, esi
-        pop esi
-        pop edi
-        pop ebx 
-    ;FIN MODO DEBUG
-
-    call sched_jugador_actual
-    
-    push eax
-    call sched_remover_tarea
-    add esp, 4
-    
-    call sched_idle
-    call sched_jmp
-
-    jmp $
-
-
-
-
+  mov eax, %1
+  imprimir_texto_mp msg%1, len%1, 0x07, 0, 0
+  jmp $
 %endmacro
 
 ;;
@@ -205,26 +113,26 @@ isrClock:            db '|/-\'
 ;;
 ;; Rutina de atención de las EXCEPCIONES
 ;; -------------------------------------------------------------------------- ;;
-ISR 0, MSG_ISR0, 12
-ISR 1, MSG_ISR1, 8
-ISR 2, MSG_ISR2, 13
-ISR 3, MSG_ISR3, 10
-ISR 4, MSG_ISR4, 8
-ISR 5, MSG_ISR5, 20
-ISR 6, MSG_ISR6, 33
-ISR 7, MSG_ISR7, 20
-ISR 8, MSG_ISR8, 12
-ISR 9, MSG_ISR9, 27
-ISR 10, MSG_ISR10, 11
-ISR 11, MSG_ISR11, 19
-ISR 12, MSG_ISR12, 19
-ISR 13, MSG_ISR13, 28
-ISR 14, MSG_ISR14, 10
-ISR 15, MSG_ISR15, 8
-ISR 16, MSG_ISR16, 13
-ISR 17, MSG_ISR17, 15
-ISR 18, MSG_ISR18, 13
-ISR 19, MSG_ISR19, 17
+ISR 0
+ISR 1
+ISR 2
+ISR 3
+ISR 4
+ISR 5
+ISR 6
+ISR 7
+ISR 8
+ISR 9
+ISR 10
+ISR 11
+ISR 12
+ISR 13
+ISR 14
+ISR 15
+ISR 16
+ISR 17
+ISR 18
+ISR 19
 
 
 ;;
